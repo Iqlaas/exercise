ffe07bc7a5de07a8b58be5305f67b26240e7b5bc
Ariff Azmi <ariff.azmi16@gmail.com>
2019-06-17 12:58:13 +0800

    add question 1

---
+++ q1.go
@@ -0,0 +1,21 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	var sum = 0;
+
+	for i := 0; i <= 1000; i++ {
+		
+		if i%3==0 || i%5==0 {
+			sum = sum+i
+			fmt.Println(i,"is in the list number can be multiply of 3 or 5")
+		}	
+	}
+
+	fmt.Println("Total sum multiply of 3 or 5 below 1000 is",sum)
+}
